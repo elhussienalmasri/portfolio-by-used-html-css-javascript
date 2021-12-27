4e7c62313574ae25371be316ebc974729257d2b4
elhussien almasri <mhmdadgjp@gmail.com>
2021-12-27 13:50:21 +0200

    portfolio by used html css javaScript

---
+++ project.js
@@ -0,0 +1,10 @@
+
+function openNav(){
+document.getElementById('mysidenav').style.width ='250px';
+document.getElementById('main').style.width ='250px';
+}
+
+function closeNav(){
+document.getElementById('mysidenav').style.width='0px';
+document.getElementById('main').style.width='0px';
+}
